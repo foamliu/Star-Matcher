ba2b9c1a425dccd2597e3cf4854c64c13dc5c879
Yang Liu <liuyang12@focusmedia.cn>
2019-05-23 17:38:11 +0800

    update

--- gen_features.py
+++ gen_features.py
@@ -6,7 +6,15 @@ import torch
 from tqdm import tqdm
 
 from config import device, pickle_file
-from utils import get_image
+from utils import get_image, FaceNotFoundError
+
+
+def count_files(stars):
+    num_files = 0
+    for star in stars:
+        num_files += len(star['file_list'])
+    return num_files
+
 
 if __name__ == "__main__":
     checkpoint = 'BEST_checkpoint.tar'
@@ -19,20 +27,33 @@ if __name__ == "__main__":
     with open('stars.json', encoding='utf-8') as file:
         stars = json.load(file)
 
+    num_files = count_files(stars)
+    print('num_files: ' + str(num_files))
+
     for star in tqdm(stars):
         name = star['name']
         file_list = star['file_list']
         feature_list = []
+        to_remove = []
         for f in file_list:
-            img = get_image(f)
-            imgs = torch.zeros([1, 3, 112, 112], dtype=torch.float)
-            imgs[0] = img
-            with torch.no_grad():
-                output = model(imgs)
-                feature = output[0].cpu().numpy()
-                x = feature / np.linalg.norm(feature)
-                feature_list.append(x)
+            try:
+                img = get_image(f)
+                imgs = torch.zeros([1, 3, 112, 112], dtype=torch.float)
+                imgs[0] = img
+                with torch.no_grad():
+                    output = model(imgs)
+                    feature = output[0].cpu().numpy()
+                    x = feature / np.linalg.norm(feature)
+                    feature_list.append(x)
+            except FaceNotFoundError:
+                to_remove.append(f)
+
+        for f in to_remove:
+            star['file_list'].remove(f)
         star['feature_list'] = feature_list
 
     with open(pickle_file, 'wb') as file:
         pickle.dump(stars, file)
+
+    num_files = count_files(stars)
+    print('num_files: ' + str(num_files))
