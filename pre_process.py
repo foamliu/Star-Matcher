4fe5ad46c19d6329e32a70fb0495d13876f87aa9
Yang Liu <liuyang12@focusmedia.cn>
2019-06-04 15:24:20 +0800

    update

--- pre_process.py
+++ pre_process.py
@@ -1,7 +1,7 @@
 import json
 import os
 from shutil import copyfile
-
+from tqdm import tqdm
 from config import IMG_FOLDER
 
 if __name__ == "__main__":
@@ -10,7 +10,11 @@ if __name__ == "__main__":
     stars = []
     dir_list = [d for d in os.listdir(IMG_FOLDER_OLD) if
                 os.path.isdir(os.path.join(IMG_FOLDER_OLD, d))]
-    for i, d in enumerate(dir_list):
+
+    num_files = 0
+
+    for i in tqdm(range(len(dir_list))):
+        d = dir_list[i]
         dir = os.path.join(IMG_FOLDER_OLD, d)
         new_dir = os.path.join(IMG_FOLDER, str(i))
         if not os.path.isdir(new_dir):
@@ -23,8 +27,12 @@ if __name__ == "__main__":
             dst = os.path.join(new_dir, f.replace(d + '_', '')).replace('\\', '/')
             new_list.append(dst)
             copyfile(src, dst)
+            num_files += 1
         stars.append({'name': d, 'file_list': new_list})
 
     # print(stars)
     with open('stars.json', 'w', encoding='utf-8') as file:
         json.dump(stars, file, ensure_ascii=False, indent=4)
+
+    print('num_stars: ' + str(len(stars)))
+    print('num_files: ' + str(num_files))
